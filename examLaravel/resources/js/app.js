3be715ad61687596a22d46e0caaee7b76384a5d3
samanthaspillier <128740460+samanthaspillier@users.noreply.github.com>
2024-08-04 19:15:12 +0200

    dependencies and welcome page
    check for duplicates, removal of tailwinds and sass and test content of welcome page to test it all

--- examLaravel/resources/js/app.js
+++ examLaravel/resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import '../css/app.css'; // Adjust the path to your CSS file
+
+
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ examLaravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Malawian Tours') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app">
+        @include('partials.header')
+
+        <main class="py-4">
+            <div class="container">
+                <h1>@yield('title')</h1>
+            @yield('content')
+
+
+            </div>
+        </main>
+    </div>
+</body>
+</html>
