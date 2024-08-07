cc138c511661a1287a2a0c9b2c7e402256152ec9
samanthaspillier <128740460+samanthaspillier@users.noreply.github.com>
2024-08-07 20:41:45 +0200

    comment model and controller
    create links with post and user in Comment, display newComment view and storeComment functions in Comment Controller

--- examLaravel/app/Http/Controllers/CommentController.php
+++ examLaravel/app/Http/Controllers/CommentController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateCommentRequest;
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    /**
+     * got to "new comment" page
+     */
+
+     public function newComment(Post $post): View
+     {
+         return view('content.newComment', compact('post'));  
+     }
+
+    /**
+     * Create a new comment.
+     */
+    public function storeComment(CreateCommentRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        Comment::create([
+            'content' => $validated['content'],
+            'user_id' => auth()->id(),
+            'post_id' => $validated['post_id'],
+        ]);
+
+        return redirect()->back()->with('success', 'Comment created successfully');
+    }
+
+    
 }
